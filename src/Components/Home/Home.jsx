d1e310f77667eb19ac069f71433fda04a3508b92
ManausK <1905246@kiit.ac.in>
2023-06-13 21:26:52 +0530

    Home section done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import video from "../../assets/video.mp4";
+import aeroplane from "../../assets/TakeOff.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home flex container">
+      <div className="mainText">
+        <h1>Create Ever-lasting Memories With Us</h1>
+      </div>
+      <div className="homeImages flex">
+        <div className="videoDiv">
+          <video src={video} autoplay muted loop className="video"></video>
+        </div>
+        <img src={aeroplane} className="plane" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search container section">
+      <div className="sectionContainer grid">
+        <div className="btns flex">
+          <div className="singleBtn">
+            <span>Economy</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
